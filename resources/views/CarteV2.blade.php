94f1c125f24dbe608f00f776744d8cfb8138542c
Jean Bentayou <jean.bentayou@iutbayonne.univ-pau.fr>
2018-05-15 14:46:00 +0000

    fnif

--- resources/views/CarteV2.blade.php
+++ resources/views/CarteV2.blade.php
@@ -84,7 +84,7 @@
 		    el.className = 'marker';
 		    
 		    //el.style.background= 'url(https://au-coeur-des-fetes-jbentayou.c9users.io/images/'+picto+') no-repeat center fixed';
-		    el.style.background= 'url('+lienPicto+') no-repeat center fixed';
+		    el.style.background= 'url('+lienPicto+') no-repeat center';
 		    //$(el).attr('data-content',"<img src='url(https://au-coeur-des-fetes-jbentayou.c9users.io/images/Icon-Fils.png)' />");
 		    //$(el).before('<img src="http://dailypost.ng/wp-content/uploads/2018/02/Efe.jpg" class="flickr" />');
 		    el.style.backgroundSize= 'contain';
@@ -157,10 +157,10 @@
 					    el.className = 'marker';
 					    
 					    //el.style.background= 'url(https://au-coeur-des-fetes-jbentayou.c9users.io/images/'+picto+') no-repeat center fixed';
-					    el.style.background= 'url('+lienPicto+') no-repeat center fixed';
+					    el.style.background= 'url('+lienPicto+')';
 					    //$(el).attr('data-content',"<img src='url(https://au-coeur-des-fetes-jbentayou.c9users.io/images/Icon-Fils.png)' />");
 					    //$(el).before('<img src="http://dailypost.ng/wp-content/uploads/2018/02/Efe.jpg" class="flickr" />');
-					    el.style.backgroundSize= 'contain';
+					    //el.style.backgroundSize= 'contain';
 					    el.style.width = '45px';
 					    el.style.height = '65px';
 					    
